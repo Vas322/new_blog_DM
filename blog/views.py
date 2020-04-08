bda5c48a7de0e90eaf267033910b6dc6d76e7435
vasily <artur.dvo@mail.ru>
2020-04-09 00:12:58 +0300

    added views delete/publish/draft posts and modify templates

--- blog/views.py
+++ blog/views.py
@@ -22,13 +22,12 @@ def post_detail(request, pk):
 
 
 def post_new(request):
+    """Вьюха для создания нового поста поста"""
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
-
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -37,15 +36,35 @@ def post_new(request):
 
 
 def post_edit(request, pk):
+    """Вьюха для редактирования поста"""
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_draft_list(request):
+    """Вьюха, которая показывает не опубликованные посты"""
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+
+def post_publish(request, pk):
+    """Вьюха, которая публикует запись"""
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+
+def post_remove(request, pk):
+    """Вьюха, которая удаляет пост"""
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
