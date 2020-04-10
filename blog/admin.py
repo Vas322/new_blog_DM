3a5f71ab55844ec75f16ba5bcf95bc9e3629cdab
vasily <artur.dvo@mail.ru>
2020-04-10 13:34:52 +0300

    add comments in blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from blog.models import Post
+from django.utils import timezone
+from blog.models import Post, Comment
 
-"""
-class PostInline(admin.TabularInline):
-    model = Post
+
+class CommentInline(admin.TabularInline):
+    model = Comment
     extra = 3
-"""
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -17,7 +16,7 @@ class PostAdmin(admin.ModelAdmin):
         ('Информация о дате создания поста', {'fields': ['created_date']}),
         ('Информация о дате публикации поста', {'fields': ['published_date']}),
     ]
-    # inlines = [PostInline]
+    inlines = [CommentInline]
     list_display = ('title', 'created_date',
                     'published_date', 'author')  # список полей в таблице
     list_filter = ['created_date']  # боковая панель с фильтром по дате
@@ -25,4 +24,17 @@ class PostAdmin(admin.ModelAdmin):
     search_fields = ['title']  # добавляем поиск по свойству title
 
 
+"""
+class CommentAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Введите автора', {'fields': ['author']}),
+        ('Введите название поста', {'fields': ['title']}),
+
+    ]
+    # inlines = [PostInline]
+    list_display = ('title', 'published_date', 'author')  # список полей в таблице
+    search_fields = ['title']  # добавляем поиск по свойству title
+
+"""
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 
 class Post(models.Model):
     """Модель описывает пост"""
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, )
     title = models.CharField('Название поста', max_length=200)
     text = models.TextField('Текст поста')
     created_date = models.DateTimeField('Дата создания поста', default=timezone.now)
@@ -27,4 +27,30 @@ class Post(models.Model):
         self.save()
 
     def __str__(self):
+        return self.text
+
+
+class Comment(models.Model):
+    """Модель комментария"""
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField('Автор комментария', max_length=100)
+    text = models.TextField('Тест комментария')
+    created_date = models.DateTimeField('Дата создания поста', default=timezone.now())
+    approved_comment = models.BooleanField(default=False)
+
+    class Meta:
+        """Атрибут, позволяющий использовать форму множественного числа 'Комментарии' """
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
+    def approve(self):
+        """Метод, подтверждающий публикацию комментарий"""
+        self.approved_comment = True
+        self.save()
+
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
+    def __str__(self):
+        """Метод возвращает строковое представление модели"""
         return self.text
